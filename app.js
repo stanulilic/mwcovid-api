08d1223b2a8e21c649e7cdc670b846616daf1873
stanley <stanleyulili@gmail.com>
2020-06-11 20:45:19 +0000

    (feat): get nation wide covid data from mwhealth api

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3001;
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+
+const getLocalCovidData = async () => {
+	try {
+		// 
+		const response = await axios({url:'https://covid19.health.gov.mw:3000/api/v0/aggregates',
+			method: 'get',
+		});
+		const result = await response.data;
+		return result 	}
+	catch(error) {
+		console.log(error);
+	}
+}
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	getLocalCovidData().then((response) => {
+	res.send(response);
+	});
 });
 
 app.listen(port, () => console.log(`Covid API listening on ${port}!`));
