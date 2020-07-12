66e3d72a986ab8b8698847cc9c1dcd201fb54dea
stanley <stanleyulili@gmail.com>
2020-07-12 18:21:16 +0000

    (feat): fix crosssite origin issues

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const { getLocalCovidData, saveCountryData, saveDistrictData}   = require('./auto-queries.js');
 const db  = require('./queries.js');
 const cron = require('node-cron');
 const app = express();
 const port = 3001;
 
-
+app.use(cors({origin: 'http://localhost:3000'}));
 app.get('/api/confirmed', db.getCountryData);
 app.get('/api/daily', db.getRecentCountryData);
 app.get('/api/districts', db.getDistrictsData);
 app.get('/api/districts/daily', db.getRecentDistrictsData);
 
-cron.schedule("1 0 * * *", function() {   
+/* 59 21 is 23:59 in malawi*/
+cron.schedule("59 21 * * *", function() {   
 saveCountryData();
 saveDistrictData();
 });
--- queries.js
+++ queries.js
@@ -1,48 +1,60 @@
-const { pool } = require('./auto-queries.js');
+const { pool } = require("./auto-queries.js");
 
 const getCountryData = (request, response) => {
-        pool.query('SELECT * FROM nationaldata', (error, results) => {
-                if(error) {
-                        throw error;
-                }
-                response.status(200).json(results.rows);
-        });
+  pool.query("SELECT * FROM nationaldata", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
 };
 
-
 const getRecentCountryData = (request, response) => {
-        pool.query('SELECT * FROM nationaldata ORDER BY id DESC LIMIT 1', (error, results) => {
-                if(error) {
-                        throw error;
-                }
-                response.status(200).json(results.rows);
-        });
-
-}
+  pool.query(
+    "SELECT * FROM nationaldata ORDER BY id DESC LIMIT 1",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const getDistrictsData = (request, response) => {
-        pool.query('SELECT districtgeolocationlat,districtGeolocationlng,districtName,numberOfConfirmedCases,numberOfConfirmedDeaths,numberOfRecoveredPatients,numberOfSuspectedCases, dateadded  FROM districtdata GROUP BY dateadded,  districtgeolocationlat,districtGeolocationlng,districtName,numberOfConfirmedCases,numberOfConfirmedDeaths,numberOfRecoveredPatients,numberOfSuspectedCases ORDER BY dateadded', (error, results) => {
-                if(error){
-                        throw error;
-                }
-                response.status(200).json(results.rows);
-        });
-}
+  pool.query(
+    "SELECT districtgeolocationlat,districtGeolocationlng,districtName,numberOfConfirmedCases,numberOfConfirmedDeaths,numberOfRecoveredPatients,numberOfSuspectedCases, dateadded  FROM districtdata GROUP BY dateadded,  districtgeolocationlat,districtGeolocationlng,districtName,numberOfConfirmedCases,numberOfConfirmedDeaths,numberOfRecoveredPatients,numberOfSuspectedCases ORDER BY dateadded",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const getRecentDistrictsData = (request, response) => {
-        const today = new Date();
-        const date =today.getDate();
-        const year = today.getFullYear();
-        const month = today.getMonth() + 1;
-        // format date as: 2020-05-30
-        const fullDate = `${year}-${month <= 9 ? `0${month}`:month}-${date}`; 
-        pool.query('SELECT * FROM districtdata where dateadded::date =$1', [fullDate], (error, results) => {
-                if(error) {
-                        throw error;
-                }
-                response.status(200).json(results.rows);
-        });
-}
+  const today = new Date();
+  const date = today.getDate();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  // format date as: 2020-05-30
+  const fullDate = `${year}-${month <= 9 ? `0${month}` : month}-${date}`;
+  pool.query(
+    "SELECT * FROM districtdata where dateadded::date =$1",
+    [fullDate],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
-
-module.exports = { getCountryData, getRecentCountryData, getDistrictsData, getRecentDistrictsData };
+module.exports = {
+  getCountryData,
+  getRecentCountryData,
+  getDistrictsData,
+  getRecentDistrictsData,
+};
